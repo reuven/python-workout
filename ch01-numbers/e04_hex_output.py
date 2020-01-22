85d1170772c2f9d4d72a5bdc653e75ad5969bc9e
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-22 20:28:21 +0200

    Chapter 1 code is written and tested!

--- ch01-numbers/e04_hex_output.py
+++ ch01-numbers/e04_hex_output.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+"""Ask the user for a hex number, and convert it to decimal"""
 
-d = 0
-h = input("Enter a hex number to convert to decimal: ")
-for power, digit in enumerate(reversed(h)):
-    d += int(digit, 16) * (16 ** power)
-print(d)
+
+def hex_output():
+    """Ask the user to enter a valid hexadecimal number, and print the decimal equivalent."""
+
+    d = 0
+    h = input("Enter a hex number to convert to decimal: ")
+    for power, digit in enumerate(reversed(h)):
+        d += int(digit, 16) * (16 ** power)
+    print(d)
---
+++ ch01-numbers/test_e04_hex_output.py
@@ -0,0 +1,28 @@
+from e04_hex_output import hex_output
+from io import StringIO
+import pytest
+
+
+def test_no_input(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', StringIO('\n'))
+    hex_output()
+    captured_out, captured_err = capsys.readouterr()
+    assert captured_out.endswith("0\n")
+
+
+def test_bad_input(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', StringIO('q\n'))
+    with pytest.raises(ValueError):
+        hex_output()
+
+
+@pytest.mark.parametrize('user_input, output', [
+    ('123', '291'),
+    ('ff', '255'),
+    ('abc123', '11256099')
+])
+def test_good_input(user_input, output, monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', StringIO(user_input + '\n'))
+    hex_output()
+    captured_out, captured_err = capsys.readouterr()
+    assert captured_out.endswith(f"{output}\n")
