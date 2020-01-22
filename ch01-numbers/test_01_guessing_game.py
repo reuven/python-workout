6eed1f0573e68c5049f1cfd984f9b17b3f06cea0
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-22 19:19:55 +0200

    Added tests with coverage to e01

--- ch01-numbers/test_01_guessing_game.py
+++ ch01-numbers/test_01_guessing_game.py
@@ -10,3 +10,45 @@ def test_correct(monkeypatch, capsys):
     captured_out, captured_err = capsys.readouterr()
 
     assert captured_out.endswith('Right!  The answer is 49\n')
+
+
+def test_too_low_once(monkeypatch, capsys):
+    random.seed(0)              # first randint will be 100
+    monkeypatch.setattr('sys.stdin', StringIO('1\n49\n'))
+    guessing_game()
+    captured_out, captured_err = capsys.readouterr()
+
+    assert 'Your guess of 1 is too low!' in captured_out
+    assert captured_out.endswith('Right!  The answer is 49\n')
+
+
+def test_too_high_once(monkeypatch, capsys):
+    random.seed(0)              # first randint will be 100
+    monkeypatch.setattr('sys.stdin', StringIO('100\n49\n'))
+    guessing_game()
+    captured_out, captured_err = capsys.readouterr()
+
+    assert 'Your guess of 100 is too high!' in captured_out
+    assert captured_out.endswith('Right!  The answer is 49\n')
+
+
+def test_too_low_twice(monkeypatch, capsys):
+    random.seed(0)              # first randint will be 100
+    monkeypatch.setattr('sys.stdin', StringIO('1\n2\n49\n'))
+    guessing_game()
+    captured_out, captured_err = capsys.readouterr()
+
+    assert 'Your guess of 1 is too low!' in captured_out
+    assert 'Your guess of 2 is too low!' in captured_out
+    assert captured_out.endswith('Right!  The answer is 49\n')
+
+
+def test_too_high_twice(monkeypatch, capsys):
+    random.seed(0)              # first randint will be 100
+    monkeypatch.setattr('sys.stdin', StringIO('100\n80\n49\n'))
+    guessing_game()
+    captured_out, captured_err = capsys.readouterr()
+
+    assert 'Your guess of 100 is too high!' in captured_out
+    assert 'Your guess of 80 is too high!' in captured_out
+    assert captured_out.endswith('Right!  The answer is 49\n')
